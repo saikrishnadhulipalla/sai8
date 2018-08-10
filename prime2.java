dfb888dfcad8bc3de9b6a4c58b897d65d7f5313c
saikrishnadhulipalla <41868252+saikrishnadhulipalla@users.noreply.github.com>
2018-08-10 11:53:19 +0530

    Rename prime1.java to prime2.java

---
+++ prime2.java
@@ -0,0 +1,32 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int count=0;
+		 for(int i=a+1;i<b;i++)
+		 {
+		 	
+			for(int j=1;j<=i;j++)
+			{
+		 	    if(i%j==0)
+				{
+					count+=1;
+				}
+			 }
+			 if(count==2)
+				System.out.print(i+" ");
+			count=0;	
+		 }
+		 
+	}
+}
