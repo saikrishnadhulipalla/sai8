af75b4cdfe3ad2f54921b76c6f81edaa784ee62e
saikrishnadhulipalla <41868252+saikrishnadhulipalla@users.noreply.github.com>
2018-08-10 12:58:26 +0530

    Create Armstrong1.java

---
+++ Armstrong1.java
@@ -0,0 +1,34 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 Scanner s = new Scanner (System.in);
+       int ch = s.nextInt();
+       int ra = s.nextInt();
+       int re,sum=0,k,m;
+         for (k = ch; k < ra; k++ )
+         {
+         	 m = k;
+         	 while ( m > 0 )
+         	 {
+         	 re = m % 10;
+         	 sum = sum + (re*re*re);
+         	 m = m /10;
+         	 }
+         	 if (sum==k)
+         	 {
+		System.out.print(sum+" ");
+         	 }
+            sum =0;
+         	 
+         }
+          
+	}
+	}
